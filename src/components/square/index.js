fd6d273efb2361135e425ba46595d0cda34598c7
zhongyangxun <zhongyangxun@163.com>
2019-10-01 14:16:12 +0800

    :construction: Add auto-open for mine square

--- src/components/square/index.js
+++ src/components/square/index.js
@@ -1,12 +1,35 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
 import './index.scss'
 
-function handleSquareClick(roundMineNum) {
-  console.log(roundMineNum)
-}
-
-export default function Square(props) {
+function Square(props) {
+  const mineMarkDisplay = props.open ? 'block' : 'none'
+  const MineMarkWrapper = styled.div`
+    display: ${mineMarkDisplay};
+    text-align: center;
+  `
   return (
-    <div className="square" onClick={() => { handleSquareClick(props.roundMineNum) }} />
+    <div className="square" onClick={() => { props.onSquareClick() }} >
+      <MineMarkWrapper>
+        {props.mineMark}
+      </MineMarkWrapper>
+    </div>
   )
 }
+
+Square.propTypes = {
+  mineMark: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.string
+  ]),
+  onSquareClick: PropTypes.func,
+  open: PropTypes.bool
+}
+
+Square.defaultProps = {
+  mineMark: 0,
+  open: false
+}
+
+export default Square
--- src/containers/mine-pane/index.js
+++ src/containers/mine-pane/index.js
@@ -1,34 +1,107 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import Square from 'components/square'
 import initMinePane from 'data/mine-pane'
 
-export default function MinewPane() {
-  const ROW = 9
-  const MINE_NUM = 16
+class MinePane extends React.Component {
+  static propTypes = {
+    rowNum: PropTypes.number,
+    mineNum: PropTypes.number
+  }
 
-  const minePane = initMinePane(ROW, MINE_NUM)
+  static defaultProps = {
+    rowNum: 9,
+    mineNum: 10
+  }
 
-  const items = []
-
-  for (let i = 0; i < ROW; i++) {
-    for (let j = 0; j < ROW; j++) {
-      items.push(
-        <Square key={i * ROW + j} roundMineNum={minePane[i][j]} />
-      )
+  constructor(props) {
+    super(props)
+    this.state = {
+      minePane: initMinePane(props.rowNum, props.mineNum),
+      openFlags: this.initopenFlags(props.rowNum)
     }
   }
 
-  const Pane = styled.div`
-    display: grid;
-    grid-template-columns: repeat(${ROW}, 30px);
-    grid-template-rows: repeat(${ROW}, 30px);
-    justify-content: center;
-    margin: 100px auto;
-  `
-  return (
-    <Pane>
-      {items}
-    </Pane>
-  )
+  initopenFlags(row = this.props.rowNum) {
+    const openFlags = []
+    for (let i = 0; i < row; i++) {
+      openFlags[i] = []
+      for (let j = 0; j < row; j++) {
+        openFlags[i][j] = false
+      }
+    }
+
+    return openFlags
+  }
+
+  openAround(i, j) {
+    this.open(i - 1, j - 1)
+    this.open(i - 1, j)
+    this.open(i - 1, j + 1)
+
+    this.open(i, j - 1)
+    this.open(i, j + 1)
+
+    this.open(i + 1, j - 1)
+    this.open(i + 1, j)
+    this.open(i + 1, j + 1)
+  }
+
+  open(i, j) {
+    const row = this.state.openFlags[i]
+
+    if (row === undefined || row[j] === undefined || row[j]) {
+      return
+    }
+
+    this.setState((prevState) => {
+      const { openFlags } = prevState
+      openFlags[i][j] = true
+
+      return {
+        openFlags
+      }
+    }, () => {
+      const { minePane } = this.state
+      if (minePane[i][j] === 0) {
+        this.openAround(i, j)
+      }
+    })
+  }
+
+  handleSquareClick(i, j) {
+    this.open(i, j)
+  }
+
+  render() {
+    const items = []
+    for (let i = 0; i < this.props.rowNum; i++) {
+      for (let j = 0; j < this.props.rowNum; j++) {
+        items.push(
+          <Square
+            key={i * this.props.rowNum + j}
+            mineMark={this.state.minePane[i][j]}
+            open={this.state.openFlags[i][j]}
+            onSquareClick={() => { this.handleSquareClick(i, j) }}
+          />
+        )
+      }
+    }
+
+    const Pane = styled.div`
+      display: grid;
+      grid-template-columns: repeat(${this.props.rowNum}, 30px);
+      grid-template-rows: repeat(${this.props.rowNum}, 30px);
+      justify-content: center;
+      margin: 100px auto;
+    `
+    return (
+      <Pane>
+        {items}
+      </Pane>
+    )
+  }
 }
+
+export default MinePane
